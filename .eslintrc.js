4d41c0ceee7cae7a1097ca6c75f17336462f5a04
Cory Pickrel <cpickrel4@gmail.com>
2019-03-17 13:55:04 -0500

    chore: configure webpack with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,9 @@ module.exports = {
     "plugins": [
         "react"
     ],
+    "parser": "babel-eslint",
     "rules": {
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error"
     }
 };
