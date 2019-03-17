e42a0f82baaca6886d7728cc63001ba325adda40
Cory Pickrel <cpickrel4@gmail.com>
2019-03-17 13:27:39 -0500

    feature: add basic react index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = "Learn GIT";
+
+ReactDOM.render(
+    <div>{title}</div>,
+    document.getElementById('app')
+);
+
+module.hot.accept();
